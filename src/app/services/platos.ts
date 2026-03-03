57cb1581a40a95758febee87ebd95eb5e9dcbc71
alvarorc13 <alvarorioscallejas09@gmail.com>
2026-03-03 20:40:00 +0100

    Servicio de platos

---
+++ src/app/services/platos.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Plato } from '../models/plato.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PlatosService {
+  private readonly apiUrl = 'http://localhost:8080/api/v1/platos';
+
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Plato[]> {
+    return this.http.get<Plato[]>(this.apiUrl);
+  }
+
+  getAllAdmin(token: string): Observable<Plato[]> {
+    const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
+    return this.http.get<Plato[]>(`${this.apiUrl}/admin/todos`, { headers });
+  }
+
+  getByCategoria(idCategoria: number): Observable<Plato[]> {
+    return this.http.get<Plato[]>(`${this.apiUrl}/categoria/${idCategoria}`);
+  }
+
+  create(plato: Partial<Plato>, token: string): Observable<Plato> {
+    const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
+    return this.http.post<Plato>(this.apiUrl, plato, { headers });
+  }
+
+  update(id: number, plato: Partial<Plato>, token: string): Observable<Plato> {
+    const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
+    return this.http.put<Plato>(`${this.apiUrl}/${id}`, plato, { headers });
+  }
+
+  delete(id: number, token: string): Observable<void> {
+    const headers = new HttpHeaders({ Authorization: `Bearer ${token}` });
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers });
+  }
+}
