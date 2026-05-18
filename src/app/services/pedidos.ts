e19efdf5120ea0bc28d8be2b7a2ffc02d7da212f
alvarorc13 <alvarorioscallejas09@gmail.com>
2026-05-18 12:24:24 +0200

    chore: cambio menor en pedidos.ts para forzar build Vercel

--- src/app/services/pedidos.ts
+++ src/app/services/pedidos.ts
@@ -1,3 +1,4 @@
+// Cambio menor para forzar build en Vercel
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
